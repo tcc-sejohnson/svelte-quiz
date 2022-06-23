bff819533e86860f79d331789ebf147906c8d42e
S. Elliott Johnson <sejohnson@torchcloudconsulting.com>
2022-06-22 21:32:54 -0600

    feat: Tailwind

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -14,7 +15,13 @@ const config = {
 				'import.meta.env.VERCEL_ANALYTICS_ID': JSON.stringify(process.env.VERCEL_ANALYTICS_ID)
 			}
 		}
-	}
+	},
+
+	preprocess: [
+		preprocess({
+			postcss: true
+		})
+	]
 };
 
 export default config;
